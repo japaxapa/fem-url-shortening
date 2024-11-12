87e112b82ad3dec7adb37348345cb79823628735
Mario Kawakita <mario.jhk@gmail.com>
2024-11-12 18:27:00 +0000

    feat: finishing mobile

--- src/components/Shortening.tsx
+++ src/components/Shortening.tsx
@@ -1,8 +1,21 @@
-import { Box, Button, Stack, TextField } from "@mui/material";
+import {
+  Box,
+  Button,
+  Divider,
+  Paper,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { BaseSyntheticEvent, useState } from "react";
 
 export default function Shortening() {
   const [longURL, setLongURL] = useState("");
+  const [error, setError] = useState(false);
+  const [active, setActive] = useState<number | null>(null);
+  const [urls, setUrls] = useState<{ longURL: string; shortURL: string }[]>([
+    { longURL: "test", shortURL: "test" },
+  ]);
 
   const handleChange = (e: BaseSyntheticEvent) => {
     setLongURL(e.target.value);
@@ -10,24 +23,52 @@ export default function Shortening() {
 
   async function handleSubmit(e: BaseSyntheticEvent) {
     e.preventDefault();
-    await fetch("https://api-ssl.bitly.com/v4/shorten", {
-      method: "POST",
-      mode: "cors",
-      headers: {
-        Authorization: `Bearer ${import.meta.env.VITE_REACT_APP_BITLY_TOKEN}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        long_url: longURL,
-        domain: "bit.ly",
-        group_guid: `${import.meta.env.VITE_REACT_APP_GUID}`,
-      }),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log({ data });
-      });
+    if (!longURL) {
+      setError(true);
+      return;
+    }
+    try {
+      setError(false);
+      console.log({ longURL });
+      await fetch("https://api-ssl.bitly.com/v4/shorten", {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          Authorization: `Bearer ${import.meta.env.VITE_REACT_APP_BITLY_TOKEN}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          long_url: longURL,
+          domain: "bit.ly",
+          group_guid: `${import.meta.env.VITE_REACT_APP_GUID}`,
+        }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.errors) {
+            throw new Error(
+              `You have these errors in the request ${data.message}`
+            );
+          } else {
+            const newUrls = [...urls];
+            newUrls.push({ longURL: data.long_url, shortURL: data.link });
+            setUrls(newUrls);
+          }
+        });
+    } catch (e) {
+      console.error(e);
+    }
   }
+
+  const handleCopy = async (index: number) => {
+    try {
+      await navigator.clipboard.writeText(urls[index].shortURL);
+    } catch (err) {
+      console.error("Failed to copy text: ", err);
+    } finally {
+      setActive(index);
+    }
+  };
 
   return (
     <Stack>
@@ -55,9 +96,15 @@ export default function Shortening() {
                 bgcolor: "white",
                 marginBottom: "1rem",
                 borderRadius: ".5rem",
+                border: error ? "2px solid red" : "",
               }}
               value={longURL}
               onChange={handleChange}
+              error={error}
+              helperText={error ? "Please add a link" : ""}
+              slotProps={{
+                htmlInput: { sx: { top: 4 } },
+              }}
             />
             <Button
               sx={{
@@ -72,6 +119,38 @@ export default function Shortening() {
               Shorten it!
             </Button>
           </Box>
+
+          {urls.map((url, index) => (
+            <Paper
+              sx={{ marginTop: "1.5rem", padding: "1rem" }}
+              elevation={0}
+              key={`${url.longURL}-${index}`}
+            >
+              <Typography sx={{ textAlign: "start" }}>{url.longURL}</Typography>
+              <Divider sx={{ marginY: "1rem" }} />
+              <Typography
+                color="primaryCyan.main"
+                sx={{ textAlign: "start", marginBottom: "1rem" }}
+              >
+                {url.shortURL}
+              </Typography>
+              <Button
+                sx={{
+                  bgcolor:
+                    active === index
+                      ? "primaryDViolet.main"
+                      : "primaryCyan.main",
+                  color: "white",
+                  width: "100%",
+                  borderRadius: ".5rem",
+                  paddingY: ".7rem",
+                }}
+                onClick={() => handleCopy(index)}
+              >
+                {active === index ? "Copied!" : "Copy"}
+              </Button>
+            </Paper>
+          ))}
         </Stack>
       </Stack>
     </Stack>
